fd187fbd5cf3b38b40ec1e84fafe7b51d07c0e64
Benjamin J. Balter <ben.balter@github.com>
2013-05-15 18:43:49 -0400

    Remove repo checks
    Conflicts:
    	scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,15 +14,19 @@ giturl=$4
 source=$5
 build=$6
 
-# Check to see if repo exists. If not, git clone it
-if [ ! -d $source ]; then
-    echo "cloning into $giturl"
-    ssh -i $(echo $(pwd))/ssh_key git clone $giturl $source
-fi
+echo "cleaning up..."
+rm $source
+
+echo "cloning into $giturl"
+ssh -i $(echo $(pwd))/ssh_key git clone $giturl $source
 
 # Git checkout appropriate branch, pull latest code
 cd $source
+
+echo "checking out $branch..."
 git checkout $branch
+
+echo "pulling $branch..."
 git pull origin $branch
 cd -
 
